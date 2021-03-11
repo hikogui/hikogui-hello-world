a1906350def61fa87cb6d57bc4af86433beba84f
Take Vos <info@takevos.nl>
2021-03-11 14:57:59 +0100

    try again

--- CMake/PostStagingScript.cmake
+++ CMake/PostStagingScript.cmake
@@ -1,11 +1,7 @@
 
 set(STAGING_DIR "${CPACK_OUTPUT_FILE_PREFIX}/${CPACK_PACKAGE_FILE_NAME}-files")
 
-get_cmake_property(_variableNames VARIABLES)
-list (SORT _variableNames)
-foreach (_variableName ${_variableNames})
-    message(STATUS "${_variableName}=${${_variableName}}")
-endforeach()
+message(STATUS "Copying cpack staging files to '${STAGING_DIR}'")
 
 # The JSON file is not yet created, but we can copy all the staging files to
 # a stable directory to be later zipped with the JSON file.
